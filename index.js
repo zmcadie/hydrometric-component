287a60a8d3cd43cf3e824afb7ca1adb31fd8f87b
Zola <zolamcadie@gmail.com>
2019-04-03 00:36:22 -0400

    Add document fragment to optomize dom manipulation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const container = document.getElementById("container");
+const content = document.createDocumentFragment();
+
+const readout = document.createElement("div");
+readout.innerText = "0.000";
+
+content.appendChild(readout);
+container.appendChild(content);
 
 fetch("https://us-central1-hydrometric-api.cloudfunctions.net/fetchWaterLevel?stationId=08HA009&timezone=pst")
   .then(res => res.json())
-  .then(res => container.innerText = res.level);
+  .then(res => readout.innerText = res.level);
