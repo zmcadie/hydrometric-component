8538133dc65c752e3f1c6699d09ba93fc86e3b47
Zola <zolamcadie@gmail.com>
2019-04-03 18:27:01 -0400

    Tweak styling

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const container = document.getElementById("container");
 const content = document.createDocumentFragment();
 
 const gauge = document.createElement("div");
-gauge.style.cssText = "border: 1px solid #444; border-radius: 20px; height: 100px; overflow: hidden; position: relative; width: 20px;";
+gauge.style.cssText = "border: 1px solid #444; border-radius: 2px; height: 200px; overflow: hidden; position: relative; width: 50px; margin: 0 auto;";
 const level = document.createElement("div");
-level.style.cssText = "background: blue; bottom: 0; height: 0; left: 0; position: absolute; transition: height 1.5s ease-in-out; width: 20px;";
+level.style.cssText = "background: #2af8; bottom: 0; height: 0; left: 0; position: absolute; transition: height 1.5s ease-in-out; width: 50px;";
 const readout = document.createElement("div");
-readout.innerText = "0.000";
+readout.innerText = "0.000 m";
+readout.style.cssText = "text-align: center;";
 
 gauge.appendChild(level);
 content.appendChild(gauge);
@@ -16,6 +17,6 @@ container.appendChild(content);
 fetch("https://us-central1-hydrometric-api.cloudfunctions.net/fetchWaterLevel?stationId=08HA009&timezone=pst")
   .then(res => res.json())
   .then(res => {
-    level.style.height = `${(res.level / 2) * 100}px`;
-    readout.innerText = res.level
+    level.style.height = `${(res.level / 4) * 200}px`;
+    readout.innerText = `${res.level} m`
   });
